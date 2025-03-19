374d63d41dc11a6ce3547e95fddf0d4fa29beb30
Raphin1506 <98553681+Raphin1506@users.noreply.github.com>
2025-03-19 11:39:59 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,17 @@
-//alert("Fala meu Cumpaça");
+// Turning the menu hamburguer into a x ===
+
+const menuHamburguer = document.querySelector(".menu-hamburguer");
+menuHamburguer.addEventListener("click", () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector(".nav-responsive");
+    menuHamburguer.classList.toggle("change");
+
+    if (menuHamburguer.classList.contains("change")) {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+}
